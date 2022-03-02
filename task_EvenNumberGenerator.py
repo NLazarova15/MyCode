45d8306f688e31fd8fe6ba0680999598b0d9819c
NLazarova15 <94830980+NLazarova15@users.noreply.github.com>
2022-03-02 15:32:33 +0200

    Update task_EvenNumberGenerator.py

--- task_EvenNumberGenerator.py
+++ task_EvenNumberGenerator.py
@@ -1,12 +1,33 @@
-#define the generator function:
+## -----------define the generator function:----------------
+# def numbers_generator(start,end):
+# 	num  = start
+# 	while num<=end:
+# 		# yield is almost like return, but it freezes the execution
+# 		yield num
+# 		num += 1
+
+# numbers = numbers_generator(1,10)
+# # iterate over our generator:
+# for i in numbers:
+# 	print(i)
+
+### -----------  define the even numbers generator function:----------------
+
 def even_numbers_generator(start,end):
-	num  = start+1
-	while num<=end:
+    num  = start
+    while num<=end:
+        if num%2==0: 
 		# yield is almost like return, but it freezes the execution
-		yield num
-		num += 2
+            yield num
+            num += 1
+        else:
+            num += 1
+            continue
+        
+       
 
 even_numbers = even_numbers_generator(1,10)
+
 # iterate over our generator:
 for i in even_numbers:
 	print(i)
